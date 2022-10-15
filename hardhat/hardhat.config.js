65301ad20ca971c2f7ff1b3d814fe52586bf35b6
Y.M. Cheung <y.m.cheung@YMs-MacBook-Pro.local>
2022-10-15 17:44:05 +0200

    setup smart contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: process.env.VUE_APP_ALCHEMY_API_URL,
+      accounts: [ process.env.VUE_APP_PRIVATE_KEY ]
+    }
+  },
+  solidity: "0.8.9",
+  settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
 };
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,31 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  //get the signer that we will use to deploy
+  const [deployer] = await ethers.getSigners();
+  
+  //Get the NFTMarketplace smart contract object and deploy it
+  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const marketplace = await Marketplace.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await marketplace.deployed();
+  
+  //Pull the address and ABI out while you deploy, since that will be key in interacting with the smart contract later
+  const data = {
+    address: marketplace.address,
+    abi: JSON.parse(marketplace.interface.format('json'))
+  }
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log('address: ', marketplace.address)
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  //This writes the ABI and address to the marketplace.json
+  //This data is then used by frontend files to connect with the smart contract
+  fs.writeFileSync('../app/src/assets/Marketplace.json', JSON.stringify(data))
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
